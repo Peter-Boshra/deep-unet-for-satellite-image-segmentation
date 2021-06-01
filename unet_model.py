82d08c13a4bd095381e37a6415106fdbc62ea0d2
Peter Boshra <peter_boshraaa@yahoo.com>
2021-06-01 21:15:39 +0200

    Update unet_model.py

--- unet_model.py
+++ unet_model.py
@@ -3,7 +3,6 @@
 from keras.models import Model
 from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, concatenate, Conv2DTranspose, BatchNormalization, Dropout
 from keras.optimizers import Adam
-from keras.utils import plot_model
 from keras import backend as K
 
 
